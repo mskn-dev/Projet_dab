244b369bfe07c877690edd72953a4080425bc488
msknmehdi <msknmehdi@gmail.com>
2014-11-16 21:56:42 +0100

    Debut interface
    Background image
    title label

--- src/projet_dab/Main.java
+++ src/projet_dab/Main.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	static {
+        Font.loadFont(Main.class.getResource("./fonts/fontawesome-webfont.ttf").toExternalForm(), 10);
+    }
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Distributeur.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
